8d1fd7ab166f3c1dea799b031aba024856a9441c
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2021-07-16 20:11:20 +0000

    Tried customizing actions for form submission

--- actions/actions.py
+++ actions/actions.py
@@ -1,67 +1,34 @@
-import json
+from typing import Any, Text, Dict, List
 
-from pathlib import Path
-from typing import Any, Text, Dict, List, Union
-
-
-from rasa_sdk import Action, Tracker, FormValidationAction
+from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
-from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
 from rasa_sdk.forms import FormAction
-from rasa_sdk.events import EventType, AllSlotsReset
-from rasa_sdk.types import DomainDict
 
-# class ActionCancelBooking(Action):
+class reservationform(FormAction):
 
-#      def name(self) -> Text:
-#             return "action_cancel_booking"
+    def name(self) -> Text:
+        return "form_info"
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+         required_slots = ["seatcount", "section", "time"]
 
-#      def run(self, dispatcher: CollectingDispatcher,
-#              tracker: Tracker,
-#              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+         for slot_name in required_slots:
+             if tracker.slots.get(slot_name) is None:
+                 return [SlotSet("requested_slot", slot_name)]
+                 
+         return [SlotSet("requested_slot", None)]
 
-#          dispatcher.utter_message("We've cancelled your booking")
+class ActionSubmit(Action):
+    def name(self)  -> Text:
+        return "action_submit"
 
-#          return [AllSlotsReset()]
+    def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict"
+        ) -> List[Dict[Text, Any]]:
 
-
-
-# class FormDataCollect(FormAction):
-#     """
-#     This form will collect the data from user about his/her booking
-#     """
-#     def name(self) -> Text:
-#         return "form_info"
-
-#     @staticmethod
-#     def required_slots(tracker: "Tracker") -> List[Text]:
-#         print("This is working - 1")
-#         return ["name", "seatcount", "bookingtime", "category",]
-
-#     def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict[Text, Any]]]]:
-#         print("This is working - 2")
-#         return {
-#             "name": [self.from_text()],
-#             "seatcount": [self.from_text()],
-#             "bookingtime": [self.from_text()],
-#             "category": [self.from_text()],
-#             # "confirm_booking": [self.from_text()],
-#             # "seatbooked": [self.from_text()]
-#         }
-
-#     def submit(
-#         self,
-#         dispatcher: "CollectingDispatcher",
-#         tracker: "Tracker",
-#         domain: Dict[Text, Any],
-#     ) -> List[EventType]:
-
-#         print("This is working - 3")
-#         dispatcher.utter_message("Here are the information that you've entered. Do you want to book? \n\t Name: {0} \n\t Number of seats: {1} \n\t Booking Time: {2} \n\t AC/Non-AC: {3} \n\t ".format(
-#             tracker.get_slot("name"),
-#             tracker.get_slot("seatcount"),
-#             tracker.get_slot("bookingtime"),
-#             tracker.get_slot("category"),
-#         ))
-#         return []
+            dispatcher.utter_message("Thanks, great job!")
+            return []
