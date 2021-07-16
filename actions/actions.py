94af7d825b4469306b3ab966dde0a73b72556a95
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2021-07-16 06:01:24 +0000

    Completed Basic Functionality of the reservation. Now working on bug fixing and improvements

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,52 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+# import json
+
+# from pathlib import Path
+# from typing import Any, Text, Dict, List, Union
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
 # from rasa_sdk import Action, Tracker
 # from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
+# from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
+# from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
+# from rasa_sdk.forms import FormAction
+# from rasa_sdk.events import EventType
+
+
+# class FormDataCollect(FormAction):
+#     """
+#     This form will collect the data from user about his/her booking
+#     """
 #     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
+#         return "form_info"
+
+#     @staticmethod
+#     def required_slots(tracker: "Tracker") -> List[Text]:
+#         print("This is working - 1")
+#         return ["name", "seatcount", "bookingtime", "category",]
+
+#     def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict[Text, Any]]]]:
+#         print("This is working - 2")
+#         return {
+#             "name": [self.from_text()],
+#             "seatcount": [self.from_text()],
+#             "bookingtime": [self.from_text()],
+#             "category": [self.from_text()],
+#             # "confirm_booking": [self.from_text()],
+#             # "seatbooked": [self.from_text()]
+#         }
+
+#     def submit(
+#         self,
+#         dispatcher: "CollectingDispatcher",
+#         tracker: "Tracker",
+#         domain: Dict[Text, Any],
+#     ) -> List[EventType]:
+
+#         print("This is working - 3")
+#         dispatcher.utter_message("Here are the information that you've entered. Do you want to book? \n\t Name: {0} \n\t Number of seats: {1} \n\t Booking Time: {2} \n\t AC/Non-AC: {3} \n\t ".format(
+#             tracker.get_slot("name"),
+#             tracker.get_slot("seatcount"),
+#             tracker.get_slot("bookingtime"),
+#             tracker.get_slot("category"),
+#         ))
 #         return []
