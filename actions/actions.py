a85ee5a7fdc0f683e919b96855909c53b52f57be
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2021-07-17 10:12:46 +0000

    Tried to validate the input from user

--- actions/actions.py
+++ actions/actions.py
@@ -10,6 +10,35 @@ from rasa_sdk.types import DomainDict
 from rasa_sdk.events import SlotSet
 
 
+
+class ValidateReservationForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_reservation_form"
+
+    def validate_time(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate booking time value."""
+        time = tracker.get_slot('time')
+        now = datetime.now()  # current time
+        datetime_obj = dateutil.parser.parse(time[-1]) # parsing the time entered by user
+        humanDate = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')  # normalising the time entered by user for comparison
+        humanDate = datetime.strptime(humanDate,'%Y-%m-%d %H:%M:%S')  # normalising the time entered by user for comparison
+        time_obj = datetime_obj.time()  # getting the time to show to user
+        today19pm = now.replace(hour=19, minute=0, second=0, microsecond=0)   # getting the normalised time for 7 PM
+        today22pm = now.replace(hour=22, minute=0, second=0, microsecond=0)  # getting the normalised time for 10 PM)
+        print(time_obj)
+        if (today19pm < humanDate) and (humanDate < today22pm):
+            print(humanDate, today19pm, today22pm)
+            return {"time": slot_value}
+        else:
+            return {"time": None}
+            
+
 class ActionReservationForm(FormAction):
     def name(self) -> Text:
         return 'reservation_form'
@@ -31,35 +60,3 @@ class ActionReservationForm(FormAction):
         datetime_obj = dateutil.parser.parse(time[-1]) # parsing the time entered by user
         time_obj = datetime_obj.time()  # getting the time to show to user
         return [SlotSet(key = "time", value = str(time_obj))]
-
-
-class ValidateReservationForm(FormValidationAction):
-    def name(self) -> Text:
-        return "validate_reservation_form"
-
-    def run(self, dispatcher, tracker, domain):
-        print("It works")
-        return []
-
-    def validate_time(
-        self,
-        slot_value: Any,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict,
-    ) -> Dict[Text, Any]:
-        """Validate booking time value."""
-        time = tracker.get_slot('time')
-        now = datetime.now()  # current time
-        datetime_obj = dateutil.parser.parse(time[-1]) # parsing the time entered by user
-        humanDate = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')  # normalising the time entered by user for comparison
-        humanDate = datetime.strptime(humanDate,'%Y-%m-%d %H:%M:%S')  # normalising the time entered by user for comparison
-        time_obj = datetime_obj.time()  # getting the time to show to user
-        today19pm = now.replace(hour=19, minute=0, second=0, microsecond=0)   # getting the normalised time for 7 PM
-        today22pm = now.replace(hour=22, minute=0, second=0, microsecond=0)  # getting the normalised time for 10 PM)
-
-        if (today19pm < humanDate) and (humanDate < today22pm):
-            print(humanDate, today19pm, today22pm)
-            return {"time": None}
-        else:
-            return {"time": None}
