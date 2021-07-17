850cd288ed9394a2f82080acac429b1bbf44aea8
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2021-07-17 09:45:10 +0000

    Customized Behaviour of Chatbot using duckling

--- actions/actions.py
+++ actions/actions.py
@@ -1,34 +1,65 @@
+import json
+import dateutil
 from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
+from datetime import datetime
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
+from rasa_sdk.types import DomainDict
+from rasa_sdk.events import SlotSet
 
-class reservationform(FormAction):
 
+class ActionReservationForm(FormAction):
     def name(self) -> Text:
-        return "form_info"
-    def run(
-        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
-    ) -> List[EventType]:
-         required_slots = ["seatcount", "section", "time"]
+        return 'reservation_form'
 
-         for slot_name in required_slots:
-             if tracker.slots.get(slot_name) is None:
-                 return [SlotSet("requested_slot", slot_name)]
-                 
-         return [SlotSet("requested_slot", None)]
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        return ["name","seatcount","time","category",]
 
-class ActionSubmit(Action):
-    def name(self)  -> Text:
-        return "action_submit"
+    def slot_mappings(self) -> Dict[Text, Any]:
+        return {
+            "name": self.from_text(),
+            "seatcount": self.from_text(),
+            "time": self.from_entity(entity="time", intent= ["inform_time",]),
+            "category": self.from_text()
+        }
 
-    def run(
-            self,
-            dispatcher,
-            tracker: Tracker,
-            domain: "DomainDict"
-        ) -> List[Dict[Text, Any]]:
+    def submit(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict]:
+        time = tracker.get_slot('time')
+        datetime_obj = dateutil.parser.parse(time[-1]) # parsing the time entered by user
+        time_obj = datetime_obj.time()  # getting the time to show to user
+        return [SlotSet(key = "time", value = str(time_obj))]
 
-            dispatcher.utter_message("Thanks, great job!")
-            return []
+
+class ValidateReservationForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_reservation_form"
+
+    def run(self, dispatcher, tracker, domain):
+        print("It works")
+        return []
+
+    def validate_time(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate booking time value."""
+        time = tracker.get_slot('time')
+        now = datetime.now()  # current time
+        datetime_obj = dateutil.parser.parse(time[-1]) # parsing the time entered by user
+        humanDate = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')  # normalising the time entered by user for comparison
+        humanDate = datetime.strptime(humanDate,'%Y-%m-%d %H:%M:%S')  # normalising the time entered by user for comparison
+        time_obj = datetime_obj.time()  # getting the time to show to user
+        today19pm = now.replace(hour=19, minute=0, second=0, microsecond=0)   # getting the normalised time for 7 PM
+        today22pm = now.replace(hour=22, minute=0, second=0, microsecond=0)  # getting the normalised time for 10 PM)
+
+        if (today19pm < humanDate) and (humanDate < today22pm):
+            print(humanDate, today19pm, today22pm)
+            return {"time": None}
+        else:
+            return {"time": None}
